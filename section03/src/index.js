9da6a7225cdcbf8ca9e37aa8fdd978c427f5f7fa
seungsu-K <kss095@naver.com>
2024-12-20 23:37:43 +0900

    [section3] Node.js

---
+++ section03/src/index.js
@@ -0,0 +1,14 @@
+// CJS
+// const { add, sub } = require('./math');
+
+// ES module
+import multi, { add, sub } from './math.js';
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multi(1, 2));
+
+import randomColor from 'randomcolor';
+
+const color = randomColor();
+console.log(color);
---
+++ section03/src/math.js
@@ -0,0 +1,22 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
+
+// CJS
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+// ES module
+// export { add, sub };
