962a95e533b4077c4cb748e623a43811a7999b72
seungsu-K <kss095@naver.com>
2024-12-23 16:35:11 +0900

    [section5] UI 표현하기

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,18 @@
+import './Main.css';
+
 function Main() {
-  return (
-    <main>
-      <h2>main</h2>
-    </main>
-  );
+  const user = {
+    name: 'Kim',
+    isLogin: true,
+  };
+
+  if (user.isLogin) {
+    return <div className="login">로그인</div>;
+  } else {
+    return <div>로그아웃</div>;
+  }
+
+  // return <>{user.isLogin ? <div>로그인</div> : <div>로그아웃</div>}</>;
 }
 
 export default Main;
