79ad444878dee59035c3ef65b0d93afb50518516
seungsu-K <kss095@naver.com>
2024-12-27 03:33:25 +0900

    [section9] useReducer - 예시

---
+++ section09/src/App.jsx
@@ -0,0 +1,68 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import Editor from './components/Editor';
+import Header from './components/Header';
+import List from './components/List';
+import Exam from './components/Exam';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '밥 먹기',
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    // todos State 값들 중 Id가 일치하는 아이템의 isDone 프로퍼티 변경
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // targetId와 일치하는 요소만 삭제한 새로운 배열
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Exam />
+      {/* <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+    </div>
+  );
+}
+
+export default App;
