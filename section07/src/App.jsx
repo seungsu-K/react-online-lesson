23b382c8d7ef5b82d84fcf3f83db57ccf7b1d6b8
seungsu-K <kss095@naver.com>
2024-12-26 17:45:18 +0900

    [section7] useEffect

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,20 +1,31 @@
 import './App.css';
 import Controller from './conponents/Controller';
+import Even from './conponents/Even';
 import Viewer from './conponents/Viewer';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState('');
 
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
   useEffect(() => {
-    console.log(`count : ${count} / input : ${input}`);
-  }, [count, input]);
-  // 의존성 배열 (dependency array)
+    console.log('mount');
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log('update');
+  });
 
   const onClickButton = (value) => {
     setCount(count + value);
-    console.log(count);
   };
 
   return (
@@ -30,6 +41,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
