61f5f6ac65a331a38966e90078a017cc6b01f90b
seungsu-K <kss095@naver.com>
2024-12-30 01:54:55 +0900

    [section12] 함수, 커스텀 훅 분리

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -4,26 +4,14 @@ import Button from '../components/Button';
 import Editor from '../components/Editor';
 import { DiaryDispatchContext, DiaryStateContext } from '../App';
 import { useContext, useEffect, useState } from 'react';
+import useDiary from '../hooks/useDiary';
 
 function Edit() {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
-  const data = useContext(DiaryStateContext);
-  const [curDiaryItem, setCurDiaryItem] = useState();
 
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    );
-
-    if (!currentDiaryItem) {
-      window.alert('존재하지 않는 일기입니다.');
-      nav('/', { replace: true });
-    }
-
-    setCurDiaryItem(currentDiaryItem);
-  }, [params.id]);
+  const curDiaryItem = useDiary(params.id);
 
   const onClickDelete = () => {
     if (window.confirm('일기를 정말 삭제할까요?')) {
