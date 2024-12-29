b6afc68faa6247c6c66b1e508c7d0c1c6df99d68
seungsu-K <kss095@naver.com>
2024-12-29 23:28:55 +0900

    [section12] Home 페이지
    - pivotDate를 기준으로 해당 월에 생성된 item 필터링

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,11 +1,39 @@
-import { useSearchParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import DiaryList from '../components/DiaryList';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { DiaryStateContext } from '../App';
+
+const getMonthlyData = (pivotDate, data) => {
+  const begin = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  const end = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+
+  return data.filter(
+    (item) => item.createdDate >= begin && item.createdDate <= end
+  );
+};
 
 function Home() {
   const [pivotDate, setPivotDate] = useState(new Date());
+  const data = useContext(DiaryStateContext);
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+  console.log(monthlyData);
 
   const onIncreaseMonth = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
@@ -21,7 +49,7 @@ function Home() {
         leftChild={<Button text={'<'} onClick={onDecreaseMonth} />}
         rightChild={<Button text={'>'} onClick={onIncreaseMonth} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 }
