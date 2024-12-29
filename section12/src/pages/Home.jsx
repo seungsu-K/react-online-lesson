fc33924161009180cb3a680b60ccf553aa844535
seungsu-K <kss095@naver.com>
2024-12-29 23:13:18 +0900

    [section12] Home 페이지
    - Header 컴포넌트의 날짜 출력 및 변경 기능 추가

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -2,16 +2,24 @@ import { useSearchParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import DiaryList from '../components/DiaryList';
+import { useState } from 'react';
 
 function Home() {
-  const [params, setParams] = useSearchParams();
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
 
   return (
     <div>
       <Header
-        title={'2024년 2월'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={'<'} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={onIncreaseMonth} />}
       />
       <DiaryList />
     </div>
