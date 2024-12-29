14bb011e0e9a5a2cfff9f6d3897e1de185605cbd
seungsu-K <kss095@naver.com>
2024-12-30 01:22:21 +0900

    [section12] Edit 페이지
    - Editor 컴포넌트 초기 입력값 렌더링
    - id가 없는 수정페이지 navigation
    - 해당 item 삭제 기능
    - 해당 item 업데이트 기능

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,8 +1,60 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { DiaryDispatchContext, DiaryStateContext } from '../App';
+import { useContext, useEffect, useState } from 'react';
 
 function Edit() {
   const params = useParams();
-  return <div>{params.id}번 일기</div>;
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+  const [curDiaryItem, setCurDiaryItem] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert('존재하지 않는 일기입니다.');
+      nav('/', { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [params.id]);
+
+  const onClickDelete = () => {
+    if (window.confirm('일기를 정말 삭제할까요?')) {
+      onDelete(params.id);
+      nav('/', { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm('일기를 정말 수정할까요?'))
+      onUpdate(
+        input.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+    nav('/', { replace: true });
+  };
+
+  return (
+    <div>
+      <Header
+        title={'일기 수정하기'}
+        leftChild={<Button onClick={() => nav(-1)} text={'< 뒤로 가기'} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={'삭제하기'} type={'NEGATIVE'} />
+        }
+      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
 }
 
 export default Edit;
